40ff9e64522ca20f44477a716c630a64d8656c6b
Mihir Sharma <mihir.sharma@unc.edu>
2025-01-24 16:23:42 -0500

    implemented very basic bluetooth functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,59 @@
-import Image from "next/image";
+"use client"
+import { useState } from 'react';
 
 export default function Home() {
+  const [devices, setDevices] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
+
+  async function handleScan() {
+    if (typeof navigator === 'undefined' || !navigator.bluetooth) {
+      setErrorMessage('Web Bluetooth is not supported in this browser/environment.');
+      return;
+    }
+    
+    try {
+      // Request a device that advertises the battery service
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{name: "SpectraDerma"}],
+        optionalServices : [0xACEF]
+      });
+      console.log("Device:")
+      console.log(device);
+
+      const server = await device.gatt?.connect()
+      console.log("Server:")
+      console.log(server);
+
+      // Connect to GATT server
+
+      // Get the battery service
+      const service = await server.getPrimaryServices();
+      console.log("Service:");
+      console.log(service);
+
+      // Get the battery level characteristic
+      const characteristic = await service[0].getCharacteristic(0xFF01);
+      console.log("characteristic:");
+      console.log(characteristic);
+      // Read the current battery level
+      const value = await characteristic.readValue();
+
+      console.log("value")
+      console.log(value)
+      
+    } catch (error) {
+      setErrorMessage(error.toString());
+    }
+  }
+
   return (
-    <div>
-      <h1>Hello from Bai Lab!</h1>
+    <div style={{ padding: 20 }}>
+      <h1>Battery Level BLE Demo</h1>
+      <button onClick={handleScan}>Scan &amp; Get Battery Level</button>
+
+      {errorMessage && (
+        <p style={{ color: 'red' }}>Error: {errorMessage}</p>
+      )}
     </div>
   );
 }
