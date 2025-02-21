5692b382d62af7eaf8511965eaea5759c4db400c
Mihir Sharma <mihir.sharma@unc.edu>
2025-02-21 14:58:52 -0500

    fixed error on toast

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
       });
     } catch (error) {
       setErrorMessage(error.message);
-      toast.error(errorMessage);
+      toast.error(error.message);
     }
   }
 
