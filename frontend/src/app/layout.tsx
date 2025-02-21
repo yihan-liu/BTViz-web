09de5847031e9d4d5ce0da28389ba273c67c1d53
Mihir Sharma <mihir.sharma@unc.edu>
2025-02-21 14:30:54 -0500

    added toast/sonner support for error messages

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster />
+        <Toaster position="bottom-center"/>
       </body>
     </html>
   );
