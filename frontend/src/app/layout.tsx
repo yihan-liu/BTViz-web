713c755ab4384c7c7cfb1a1f362d445b65a88a2a
Mihir Sharma <mihir.sharma@unc.edu>
2025-03-21 14:00:48 -0400

    added speed insights to app

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster position="bottom-right" richColors/>
+        <SpeedInsights />
       </body>
     </html>
   );
